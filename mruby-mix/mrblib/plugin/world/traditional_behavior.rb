62397281620c5405af51faeb1cbe00d0ff05381b
shibafu <shibafu528@gmail.com>
2020-09-02 01:26:58 +0900

    add message's traditional behavior

--- mruby-mix/mrblib/plugin/world/traditional_behavior.rb
+++ mruby-mix/mrblib/plugin/world/traditional_behavior.rb
@@ -1,32 +1,44 @@
 module Plugin::World
-  # レガシーなmikutter サードパーティプラグインが、Worldインスタンスに対して暗黙に期待する振る舞いの定義
-  # これをmix-inすることで、レガシープラグインとの一定の互換性を確保できる。
-  #
-  # レガシープラグインは、当時のServiceクラスのインスタンスメソッドの振る舞いを期待している。
-  # 特にmikutter 3.6(2017-12-25)より前の時代に作成されたプラグインに多い。
-  # mikutter 4.1現在ではWorldとSpellを組み合わせて使うことで置き換えられている。
+  # レガシーなmikutter サードパーティプラグインが、Modelに対して暗黙に期待する振る舞いの定義
+  # Modelの役割に応じてmix-inすることで、レガシープラグインとの一定の互換性を確保できる。
   module TraditionalBehavior
-    def post(to: nil, message:, **kwrest)
-      Plugin[:world].compose(self, to, body: message, **kwrest)
-    end
-    alias update post # 厳密にはupdateはmikutwitterの呼び出しだが...
+    # mikutter 3.5 Service (Twitter::World) 互換
+    module World
+      def post(to: nil, message:, **kwrest)
+        Plugin[:world].compose(self, to, body: message, **kwrest)
+      end
+      alias update post # 厳密にはupdateはmikutwitterの呼び出しだが...
 
-    def service
-      self
+      def service
+        self
+      end
+
+      # @return [Diva::Model]
+      def user_obj
+        # いい感じに実装しておいてください
+        _traditional_user
+      end
+      alias to_user user_obj
+
+      # @return [String]
+      def idname
+        # いい感じに実装しておいてください
+        _traditional_idname
+      end
+      alias user idname
     end
 
-    # @return [Diva::Model]
-    def user_obj
-      # いい感じに実装しておいてください
-      _traditional_user
-    end
-    alias to_user user_obj
+    # Twitter::Message 互換
+    module Message
+      # @param [Hash] other
+      def post(message:, **kwrest)
+        world, = Plugin[:world].filtering(:current_world, nil)
+        Plugin[:world].compose(world, self, body: message, **kwrest)
+      end
 
-    # @return [String]
-    def idname
-      # いい感じに実装しておいてください
-      _traditional_idname
+      def message
+        self
+      end
     end
-    alias user idname
   end
 end
